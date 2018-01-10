9c25bedd283c3a4460e7b460288bfb2be91154f2
daballard1127 <denise_ballard@att.net>
2018-01-09 19:22:12 -0600

    Homework

--- config/connection.js
+++ config/connection.js
@@ -32,14 +32,14 @@ if (process.env.JAWSDB_URL){
 // var connection = mysql.createConnection(source.localhost);
 
 
-Connecting to the database.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// Connecting to the database.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Exporting our connection
 connection.connect();
