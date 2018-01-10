3a8c859992622dc458a7c90aed9abfa756e0daac
daballard1127 <denise_ballard@att.net>
2018-01-09 19:27:10 -0600

    Homework

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
+} else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
